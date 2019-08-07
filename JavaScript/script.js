b7538d59c3b2b0f4544c33a2c2125d3fb6dbad91
Zitao Zheng <dudruc@users.noreply.github.com>
2019-08-06 21:22:05 -0700

    initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var age = prompt("How old are you?");
+var fullName = firstName + " " + lastName;xs
+
+alert ("Your full name is" + firstName + " " + lastName);
+
+console.log(age + " years is roughly " + age*365 + " days.");
